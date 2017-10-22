a9d85c16ae4ea55f3b10aafdf435ac71c8c72ef5
tlin108 <tlin108@hotmail.com>
2017-10-22 13:02:15 -0400

    typo

--- justdesserts/src/index.js
+++ justdesserts/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import Routes from './route'
+import Routes from './routes'
 
 ReactDOM.render(
   <Routes/>, 
