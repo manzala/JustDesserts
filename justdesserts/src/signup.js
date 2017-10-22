288d68e4018298111537647426ba3eb529aedf7d
manzala <mnzl258@gmail.com>
2017-10-22 13:26:25 -0400

    set working routing for Signup.js from login page App.js

--- justdesserts/src/signup.js
+++ justdesserts/src/signup.js
@@ -3,31 +3,29 @@ import cupcake from './images/cupcake.png'
 import './App.css';
 
 class Signup extends Component {
-  render() {
-    return 'sign up'
-    // return (
-    //     <div className="ignup">
-    //        <div className = "loginBox">
-    //     <div className = "glass">
-    //       <img src= { cupcake} className = "user" />
-    //       <h3>Signup Here!</h3>
-    //       <form>
-    //           <div className = "inputBox">
-    //             <input type="text" name="" placeholder="Username" />
-    //             <span><i className="fa fa-user" aria-hidden="true"></i></span>
-    //           </div>
-    //           <div className = "inputBox">
-    //             <input type="password" name="" placeholder="Password" />
-    //             <span><i className="fa fa-lock" aria-hidden="true"></i></span>
-    //           </div>
-    //             <input type="submit" name="" value="Login" />
-    //       </form>
-    //       <a href="#">Login!</a>
-          
-    //     </div>
-    //   </div>
-    //     </div>
-    // );
+  render() { 
+    return (
+        <div className="ignup">
+            <div className = "loginBox">
+                <div className = "glass">
+                    <img src= { cupcake} className = "user" />
+                    <h3>Signup Here!</h3>
+                    <form>
+                        <div className = "inputBox">
+                            <input type="text" name="" placeholder="Username" />
+                            <span><i className="fa fa-user" aria-hidden="true"></i></span>
+                        </div>
+                        <div className = "inputBox">
+                            <input type="password" name="" placeholder="Password" />
+                            <span><i className="fa fa-lock" aria-hidden="true"></i></span>
+                        </div>
+                        <input type="submit" name="" value="Login" />
+                    </form>
+                   <a href="#">Login!</a>  
+                </div>
+            </div>
+        </div>
+    );
   }
 }
 
